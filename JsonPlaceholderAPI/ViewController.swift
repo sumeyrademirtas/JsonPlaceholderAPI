b10039a4fd14aa630de73f3a9c98387b67bff5de
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-09-03 20:16:18 +0300

    Initial Commit

---
+++ JsonPlaceholderAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonPlaceholderAPI
+//
+//  Created by Sümeyra Demirtaş on 3.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
