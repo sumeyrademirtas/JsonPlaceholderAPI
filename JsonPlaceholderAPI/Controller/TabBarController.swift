5b8caa46ba94b7f449de8367fae796ff0ab182e4
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-09-04 20:25:19 +0300

    refactor(tab-bar): removed navigation large title preferences
    # removed navigationItem.largeTitleDisplayMode settings for usersVC and searchVC
    # removed prefersLargeTitles configuration for navigation controllers in TabBarController

--- JsonPlaceholderAPI/Controller/TabBarController.swift
+++ JsonPlaceholderAPI/Controller/TabBarController.swift
@@ -7,37 +7,24 @@
 
 import UIKit
 
-
 /// Controller to house tabs and root tab controllers
 final class TabBarController: UITabBarController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         setUpTabs()
     }
-    
+
     private func setUpTabs() {
         let usersVC = UsersListVC()
         let searchVC = SearchVC()
 
-        
-        usersVC.navigationItem.largeTitleDisplayMode = .automatic
-        searchVC.navigationItem.largeTitleDisplayMode = .automatic
-
-        
         let nav1 = UINavigationController(rootViewController: usersVC)
         let nav2 = UINavigationController(rootViewController: searchVC)
-        
+
         nav1.tabBarItem = UITabBarItem(title: "Users", image: UIImage(systemName: "person.3.fill"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass.circle.fill"), tag: 2)
 
-        for nav in [nav1, nav2] {
-            nav.navigationBar.prefersLargeTitles = true
-        }
-        
         setViewControllers([nav1, nav2], animated: true)
     }
-    
-
 }
